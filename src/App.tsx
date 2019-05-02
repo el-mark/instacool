a929220da36285798a0cd9baf1e932e314703258
Mark <mharmsenr@gmail.com>
2019-05-02 14:51:02 -0300

    Create components in order to factorize App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
-import * as React from 'react';
-import './App.css';
-
-import logo from './logo.svg';
+import * as React from 'react'
+import './App.css'
+import Header from './components/Header'
+import Intro from './components/Intro'
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <Header />
+        <Intro />
+        <Intro text='Texto para intro 2'/>
       </div>
     );
   }
---
+++ src/components/Intro.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react'
+
+interface IIntroProps{
+    text?: string
+}
+
+interface IIntroState{
+    text: string
+    dato: number
+}
+
+export default class Intro extends React.Component<IIntroProps, IIntroState> {
+    public state = {
+        dato: 1,
+        text: 'Soy un texto del estado.'
+    }
+    
+    public render() {
+        
+        const { text } = this.props
+        
+        const t = text ? text : this.state.text
+        
+        return(
+            <p onClick={this.handleClick} className="App-intro">
+                <span>{ t }</span>
+            </p>
+        )
+    }
+
+    private handleClick = () => {
+        this.setState({ text: 'Me actualicé!' })
+    }
+}
