8c12e98b75c280677baa57407151e7694b0e386c
Mark <mharmsenr@gmail.com>
2019-05-03 20:18:13 -0300

    Add register view

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import * as React from 'react'
 import { Route } from 'react-router'
 import './App.css'
 import Login from './containers/Auth/Login'
+import Register from './containers/Auth/Register'
 
 class App extends React.Component {
   public render() {
     return (
       <div>
         <Route exact={true} path='/' component={Login} />
+        <Route exact={true} path='/register' component={Register} />
       </div>
     );
   }
