5f66194938dcfa5aedf9028067774f1388dfd7ed
Mark <mharmsenr@gmail.com>
2019-05-03 19:47:46 -0300

    Create sign in view

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import * as React from 'react'
 import './App.css'
+import Button from './components/Button'
 import Card from './components/Card'
+import Center from './components/Center'
 import Container from './components/Container'
+import Input from './components/Input'
+import Link from './components/Link'
+import Title from './components/Title'
 
 class App extends React.Component {
   public render() {
     return (
       <Container>
         <Card>
-          lala
+          <Title>Iniciar de sesión</Title>
+          <Input placeholder='Correo' label='Correo'/>
+          <Input placeholder='Contraseña' label='Contraseña'/>
+          <Button block={true}>Enviar</Button>
+          <Center>
+            <Link>Ir al registro</Link>
+          </Center>
         </Card>
       </Container>
     );
--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,8 +1,13 @@
 import * as React from 'react'
 
 const style = {
+    alignItems: 'center',
     backgroundColor: '#eee',
+    display: 'flex',
+    height: 'calc(100vh - 20px)',
+    justifyContent: 'center',
     padding: '10px 15px',
+    width: 'calc(100vw - 30px)',
 }
 
 export default class Container extends React.Component {
