700684bb77d18f9ae8dce30655f88f167d1b4134
Mark <mharmsenr@gmail.com>
2019-05-03 17:32:45 -0300

    Dele header and intro

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import * as React from 'react'
 import './App.css'
-import Header from './components/Header'
-import Intro from './components/Intro'
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <Header />
-        <Intro />
-        <Intro text='Texto para intro 2'/>
       </div>
     );
   }
