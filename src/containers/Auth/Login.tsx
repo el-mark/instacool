8ffa2e7079316bf12e2fff6ad0a17cc6bfa27c72
Mark <mharmsenr@gmail.com>
2019-05-04 20:17:42 -0300

    Login add redux-form, connect to redux, add thunk and types to services, add onsubmit to form and change inputs to Fields

--- src/containers/Auth/Login.tsx
+++ src/containers/Auth/Login.tsx
@@ -1,27 +1,35 @@
 import * as React from 'react'
-import { Link } from 'react-router-dom'
+import { connect } from 'react-redux';
 
-import Button from '../../components/Button'
 import Card from '../../components/Card'
-import Center from '../../components/Center'
 import Container from '../../components/Container'
-import Input from '../../components/Input'
+import LoginForm from '../../components/Loginform'
 import Title from '../../components/Title'
 
-export default class Login extends React.Component {
+import { ILogin, login as loginThunk } from '../../ducks/Users'
+
+interface ILoginProps {
+    login: (a: ILogin) => void
+}
+
+class Login extends React.Component<ILoginProps> {
     public render() {
+        const { login } = this.props
         return(
             <Container center={true} >
                 <Card>
                 <Title>Iniciar de sesión</Title>
-                <Input placeholder='Correo' label='Correo'/>
-                <Input placeholder='Contraseña' label='Contraseña'/>
-                <Button block={true}>Enviar</Button>
-                <Center>
-                    <Link to='/register'>Ir al registro</Link>
-                </Center>
+                <LoginForm onSubmit={login}/>
                 </Card>
             </Container>
         )
     }
 }
+
+const mapStateToProps = (state: any) => state
+
+const mapDispatchToProps = (dispatch: any) => ({
+    login: (payload: any) => dispatch(loginThunk(payload))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login)
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,15 @@
+import { auth, firestore, storage } from 'firebase'
+
 import * as firebase from './firebase'
 
 const services = {
     ...firebase,
+}
+
+export interface IServices {
+    db: firestore.Firestore
+    storage: storage.Storage
+    auth: auth.Auth
 }
 
 export default services
